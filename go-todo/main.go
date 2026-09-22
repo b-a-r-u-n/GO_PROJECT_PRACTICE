3631d29dbdde9767c453a136cf2e2ef807b6484b
b-a-r-u-n <barunkumarmahakud40@gmail.com>
2026-09-22 23:17:09 +0530

    Implement addTodo function and update main logic for todo management

--- go-todo/main.go
+++ go-todo/main.go
@@ -13,10 +13,21 @@ type Todo struct {
 	Completed bool
 }
 
+func addTodo(todoId int, todoTitle string, todos *[]Todo){
+	todo := Todo{
+		Id: todoId,
+		Title: todoTitle,
+		Completed: false,
+	}
+
+	*todos = append(*todos, todo)
+}
+
 func main() {
 
 	fmt.Println("========= TODO APP =========")
 
+	//                              METHOD - 1
 	// todoOne := Todo{
 	// 	Id:        1,
 	// 	Title:     "Todo one title",
@@ -41,39 +52,131 @@ func main() {
 	// 	fmt.Printf("Todo Id: %d, Todo Title: %s, Todo Completed: %t\n",todo.Id, todo.Title, todo.Completed)
 	// }
 
-	var todos []Todo
+	//                              METHOD - 2
+	// var todos []Todo
 
-	choose := "y"
-	reader := bufio.NewReader(os.Stdin)
+	// choose := "y"
+	// reader := bufio.NewReader(os.Stdin)
+	// todoId := 1
+
+	// for {
+	// 	if choose == "y" || choose == "Y" {
+	// 		fmt.Printf("Enter the todo title: ")
+	// 		todoTitle, _ := reader.ReadString('\n')
+	// 		todoTitle = strings.TrimSpace(todoTitle)
+
+	// 		todo := Todo{
+	// 			Id:        todoId,
+	// 			Title:     todoTitle,
+	// 			Completed: false,
+	// 		}
+
+	// 		todos = append(todos, todo)
+	// 		todoId++
+	// 		fmt.Println("Do you want to add new todo (y/n)")
+	// 		// fmt.Scan(&choose)
+	// 		choose, _ = reader.ReadString('\n')
+	// 		choose = strings.TrimSpace(choose)
+	// 	} else if choose == "n" || choose == "N" {
+	// 		break
+	// 	} else {
+	// 		fmt.Println("Choose a valid choice (y/n)")
+	// 		// fmt.Scan(&choose)
+	// 		choose, _ = reader.ReadString('\n')
+	// 		choose = strings.TrimSpace(choose)
+	// 	}
+	// }
+
+	// fmt.Println(todos)
+
+	// var todos []Todo
+	// todoId := 1
+	// choice := "y"
+	// reader := bufio.NewReader(os.Stdin)
+
+	// for {
+	// 	if choice == "y" || choice == "Y"{
+	// 		fmt.Printf("Enter the todo title: ")
+	// 		todoTitle, readerErr := reader.ReadString('\n')
+	// 		if readerErr != nil {
+	// 			fmt.Println("Error while reading |", readerErr)
+	// 			return
+	// 		}
+	// 		todoTitle = strings.TrimSpace(todoTitle)
+	// 		addTodo(todoId, todoTitle, &todos)
+
+	// 		fmt.Printf("Do you want to add another todo (y/n): ")
+	// 		var choiceErr error
+	// 		choice, choiceErr = reader.ReadString('\n')
+	// 		if choiceErr != nil{
+	// 			fmt.Println("Error while reading |", choiceErr)
+	// 			break
+	// 		}
+	// 		choice = strings.TrimSpace(choice)
+	// 		todoId++
+	// 	} else if choice == "n" || choice == "N"{
+	// 		break
+	// 	} else {
+	// 		fmt.Printf("Invalid choice choose again (y/n): ")
+	// 		var choiceErr error
+	// 		choice, choiceErr = reader.ReadString('\n')
+	// 		if choiceErr != nil{
+	// 			fmt.Println("Error while reading |", choiceErr)
+	// 			break
+	// 		}
+	// 		choice = strings.TrimSpace(choice)
+	// 	}
+	// }
+	// fmt.Println("Todos: ", todos)
+
+
+	var todos []Todo
 	todoId := 1
+	choice := "1"
+	reader := bufio.NewReader(os.Stdin)
 
 	for {
-		if choose == "y" || choose == "Y" {
+		if choice == "1"{
 			fmt.Printf("Enter the todo title: ")
-			todoTitle, _ := reader.ReadString('\n')
-			todoTitle = strings.TrimSpace(todoTitle)
-
-			todo := Todo{
-				Id:        todoId,
-				Title:     todoTitle,
-				Completed: false,
+			todoTitle, readerErr := reader.ReadString('\n')
+			if readerErr != nil {
+				fmt.Println("Error while reading |", readerErr)
+				return
 			}
+			todoTitle = strings.TrimSpace(todoTitle)
+			addTodo(todoId, todoTitle, &todos)
 
-			todos = append(todos, todo)
+			fmt.Println("1. Add todo")
+			fmt.Println("2. Remove todo")
+			fmt.Println("3. Update todo")
+			fmt.Println("4. Exit")
+			fmt.Printf("Enter your choice: ")
+			var choiceErr error
+			choice, choiceErr = reader.ReadString('\n')
+			if choiceErr != nil{
+				fmt.Println("Error while reading |", choiceErr)
+				break
+			}
+			choice = strings.TrimSpace(choice)
 			todoId++
-			fmt.Println("Do you want to add new todo (y/n)")
-			// fmt.Scan(&choose)
-			choose, _ = reader.ReadString('\n')
-			choose = strings.TrimSpace(choose)
-		} else if choose == "n" || choose == "N" {
+		} else if choice == "4" {
+			fmt.Println("Goodbye..")
 			break
 		} else {
-			fmt.Println("Choose a valid choice (y/n)")
-			// fmt.Scan(&choose)
-			choose, _ = reader.ReadString('\n')
-			choose = strings.TrimSpace(choose)
+			fmt.Println("Invalid choice choose again")
+			fmt.Println("1. Add todo")
+			fmt.Println("2. Remove todo")
+			fmt.Println("3. Update todo")
+			fmt.Println("4. Exit")
+			fmt.Printf("Enter your choice: ")
+			var choiceErr error
+			choice, choiceErr = reader.ReadString('\n')
+			if choiceErr != nil{
+				fmt.Println("Error while reading |", choiceErr)
+				break
+			}
+			choice = strings.TrimSpace(choice)
 		}
 	}
-
-	fmt.Println(todos)
+	fmt.Println("Todos: ", todos)
 }
